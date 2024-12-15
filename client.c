708357f327c7a44b64795fd8cabeaeeb2eed5b40
ezeppa <ezeppa@Debian.myguest.virtualbox.org>
2024-12-15 05:32:06 +0100

    Project work but speed issues

--- client.c
+++ client.c
@@ -1,9 +1,50 @@
 #include "libft.h"
 #include <signal.h>
 
+void	send_char(int pid, char c)
+{
+	char	bit;
+	char	mask;
+
+	bit = 0;
+	mask = 0b10000000;
+	while (bit < 8)
+	{
+		if ((c & mask) != mask)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		c <<= 1;
+		bit++;
+		usleep(100);
+	}
+}
+
+void	send_message(int pid, char *msg)
+{
+	while (*msg)
+	{
+		send_char(pid, *msg);
+		msg++;
+	}
+	send_char(pid, '\0');
+}
+
 int	main(int argc, char *argv[])
 {
-	int	pid = ft_atoi(argv[1]);
-	kill(pid, SIGUSR1);
+	int	pid;
+	int	i;
+
+	if (argc != 3)
+		return (1);
+	i = 0;
+	while (i < ft_strlen(argv[1]))
+	{
+		if (!ft_isdigit(argv[1][i]))
+			return (1);
+		i++;
+	}
+	pid = ft_atoi(argv[1]);
+	send_message(pid, argv[2]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -1,27 +1,23 @@
 #include <signal.h>
 #include "libft.h"
 
-#define BUFFER_SIZE 64
+#define BUFFER_SIZE 128
 
-char	*buffer;
+typedef struct s_message
+{
+	char	*buffer;
+	int		char_i;
+	int		bit_i;
+}	t_message;
+
+t_message	message;
 
 void	add_bit_buffer(char bit)
 {
-	static int	char_i = 0;
-	static int	bit_i = 0;
-
-	if (bit_i == 8)
-	{
-		if (buffer[char_i] == '\0')
-			char_i = 0;
-		else
-			char_i++;
-		bit_i = 0;
-	}
-	buffer[char_i] <<= 1;
+	message.buffer[message.char_i] <<= 1;
 	if (bit)
-		buffer[char_i]++;
-	bit_i++;
+		message.buffer[message.char_i]++;
+	message.bit_i++;
 }
 
 void	bit_0()
@@ -36,40 +32,41 @@ void	bit_1()
 
 void	wait_signal()
 {
+	signal(SIGUSR1, bit_0);
+	signal(SIGUSR2, bit_1);
 	while (TRUE)
 	{
-		signal(SIGUSR1, bit_0);
-		signal(SIGUSR2, bit_1);
 		pause();
-		if (ft_strnchr(buffer, '\0', BUFFER_SIZE))
+		if (message.char_i > BUFFER_SIZE)
 		{
-			ft_printf("Message recu : %s\n", buffer);
-			ft_memset((void *)&buffer[0], '\0', BUFFER_SIZE);
+			ft_printf("Message corrompu !\n");
+			message.char_i = 0;
+			message.bit_i = 0;
+		}
+		if (message.bit_i == 8 && message.buffer[message.char_i] == '\0')
+		{
+			ft_printf("Message recu : %s\n", message.buffer);
+			// free(message.buffer);
+			// message.buffer = ft_calloc(sizeof(char), BUFFER_SIZE);
+			message.char_i = 0;
+			message.bit_i = 0;
+		}
+		if (message.bit_i == 8)
+		{
+			message.bit_i = 0;
+			message.char_i++;
+			message.buffer[message.char_i] = '\0';
 		}
 	}
 }
 
 int	main(int argc, char *argv[])
 {
-	buffer = ft_calloc(sizeof(char), BUFFER_SIZE);
+	message.buffer = ft_calloc(sizeof(char), BUFFER_SIZE);
+	message.char_i = 0;
+	message.bit_i = 0;
 	ft_printf("Server PID : %d\n", getpid());
-	
-
-
-	char	*str = "test";
-	while (*str)
-	{
-		char	byte = *str;
-		char	bit = 0;
-		while (bit < 8)
-		{
-			add_bit_buffer((byte & 0b10000000) == 0b10000000 ? (char)0b00000001 : 0);
-			byte <<= 1;
-			bit++;
-		}
-		str++;
-	}
-
-
+	wait_signal();
+	free(message.buffer);
 	return (0);
 }
