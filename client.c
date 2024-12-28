9ec72678c43831db7cbaa0495ec07001bd691d99
ezeppa <ezeppa@Debian.myguest.virtualbox.org>
2024-12-28 16:42:14 +0100

    ADD sigaction on server side

--- client.c
+++ client.c
@@ -2,76 +2,48 @@
 #include <signal.h>
 #include <fcntl.h>
 
-int	is_bit_received;
-
-void	send_pid(int pid_server)
+typedef struct s_client
 {
-	int	pid;
-	int	bits;
-	int	bit;
+	int		pid_server;
+	char	*msg;
+	int		bit;
+}	t_client;
 
-	pid = getpid();
-	bits = sizeof(int) * 8;
-	while (--bits >= 0)
+t_client	client;
+
+void	send_next_bit()
+{
+	int	next_bit;
+
+	next_bit = (client.msg[0] >> (7 - client.bit)) & (1 << 7);
+	client.msg[0] <<= 1;
+	client.bit++;
+	if (client.bit == 8)
 	{
-		bit = (pid >> bits) & 1;
-		if (!bit)
-			kill(pid_server, SIGUSR1);
-		else
-			kill(pid_server, SIGUSR2);
-		usleep(1000);
+		client.bit = 0;
+		client.msg++;
 	}
+	if (next_bit)
+		kill(client.pid_server, SIGUSR2);
+	else
+		kill(client.pid_server, SIGUSR1);
 }
 
-void	handle(){
-	is_bit_received = 1;
-}
-
-void	send_char(int pid, char c)
+void	s1_handler()
 {
-	char	bit;
-	char	mask;
-
-	bit = 0;
-	mask = 0b10000000;
-	while (bit < 8)
-	{
-		if ((c & mask) != mask)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		c <<= 1;
-		bit++;
-		if (!is_bit_received)
-			pause();
-		is_bit_received = 0;
-	}
+	send_next_bit();
 }
 
-void	send_message(int pid, char *msg)
+void	s2_handler()
 {
-	while (*msg)
-	{
-		send_char(pid, *msg);
-		msg++;
-	}
-	send_char(pid, '\0');
-}
-
-void	received()
-{
-	is_bit_received = 1;
 	ft_printf("Server has received your message !\n");
+	exit(EXIT_SUCCESS);
 }
 
 int	main(int argc, char *argv[])
 {
-	int	pid_server;
 	int	i;
 
-	is_bit_received = 0;
-	signal(SIGUSR1, handle);
-	signal(SIGUSR2, received);
 	if (argc != 3)
 		return (1);
 	i = 0;
@@ -81,8 +53,16 @@ int	main(int argc, char *argv[])
 			return (1);
 		i++;
 	}
-	pid_server = ft_atoi(argv[1]);
-	send_pid(pid_server);
-	send_message(pid_server, argv[2]);
+	signal(SIGUSR1, s1_handler);
+	signal(SIGUSR2, s2_handler);
+
+	client.pid_server = ft_atoi(argv[1]);
+	client.msg = argv[2];
+	client.bit = 0;
+
+	send_next_bit();
+
+	while (TRUE)
+		pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -10,18 +10,9 @@ typedef struct s_message
 	int		buffer_size;
 	int		char_i;
 	int		bit_i;
-	int		pid_client;
-	int		is_pending_pid_c;
 }	t_message;
 
 t_message	message;
-
-void	add_bit_pid_client(char bit)
-{
-	message.pid_client <<= 1;
-	if (bit)
-		message.pid_client++;
-}
 
 void	add_bit_buffer(char bit)
 {
@@ -31,82 +22,84 @@ void	add_bit_buffer(char bit)
 	message.bit_i++;
 }
 
-void	bit_0()
+void	alloc_buffer()
 {
-	if (message.is_pending_pid_c)
-		add_bit_pid_client(0);
-	else
-		add_bit_buffer(0);
-}
-
-void	bit_1()
-{
-	if (message.is_pending_pid_c)
-		add_bit_pid_client(1);
-	else
-		add_bit_buffer(1);
-}
-
-void	wait_pid_client()
-{
-	int	int_size;
-
-	int_size = sizeof(int) * 8;
-	while (int_size)
+	if (!message.buffer || message.char_i >= message.buffer_size - 1)
 	{
-		pause();
-		int_size--;
+		message.buffer_size = message.buffer_size + BUFFER_SIZE;
+		message.buffer = ft_realloc(message.buffer, sizeof(char) * message.buffer_size);
+		if (!message.buffer)
+		{
+			ft_printf("Erreur d'allocation de la memoire du buffer !\n");
+			exit(1);
+		}
 	}
-	message.is_pending_pid_c = 0;
 }
 
-void	wait_signal()
+void	check_buffer(int pid)
 {
-	while (TRUE)
+	if (message.bit_i == 8 && message.buffer[message.char_i] == '\0')
 	{
-		if (!message.buffer || message.char_i >= message.buffer_size - 1)
-		{
-			message.buffer_size = message.buffer_size + BUFFER_SIZE;
-			message.buffer = ft_realloc(message.buffer, sizeof(char) * message.buffer_size);
-			if (!message.buffer)
-			{
-				ft_printf("Erreur d'allocation de la memoire du buffer !\n");
-				exit(1);
-			}
-		}
-		pause();
-		if (message.bit_i == 8 && message.buffer[message.char_i] == '\0')
-		{
-			ft_printf("Message recu : %s\n", message.buffer);
-			kill(message.pid_client, SIGUSR2);
-			break;
-		}
-		if (message.bit_i == 8)
-		{
-			message.bit_i = 0;
-			message.char_i++;
-			message.buffer[message.char_i] = '\0';
-		}
-		kill(message.pid_client, SIGUSR1);
+		ft_printf("Message recu : %s\n", message.buffer);
+		kill(pid, SIGUSR2);
+		message.char_i = 0;
+		message.bit_i = 0;
+		free(message.buffer);
+		message.buffer = NULL;
 	}
+	if (message.bit_i == 8)
+	{
+		message.bit_i = 0;
+		message.char_i++;
+		message.buffer[message.char_i] = '\0';
+	}
+}
+
+void	s1_handler(int signal, siginfo_t *info, void *s)
+{
+	(void)s;
+	(void)signal;
+
+	alloc_buffer();
+	add_bit_buffer(0);
+	check_buffer(info->si_pid);
+	kill(info->si_pid, SIGUSR1);
+}
+
+void	s2_handler(int signal, siginfo_t *info, void *s)
+{
+	(void)s;
+	(void)signal;
+
+	alloc_buffer();
+	add_bit_buffer(1);
+	check_buffer(info->si_pid);
+	kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
-	signal(SIGUSR1, bit_0);
-	signal(SIGUSR2, bit_1);
+	struct sigaction	s_1;
+	struct sigaction	s_2;
+	
+	s_1.sa_sigaction = s1_handler;
+	s_2.sa_sigaction = s2_handler;
+	sigemptyset(&s_1.sa_mask);
+	sigemptyset(&s_2.sa_mask);
+	s_1.sa_flags = SA_SIGINFO;
+	s_2.sa_flags = SA_SIGINFO;
+	if ((sigaction(SIGUSR1, &s_1, NULL) == -1)
+		|| (sigaction(SIGUSR2, &s_2, NULL) == -1))
+		exit(EXIT_FAILURE);
+
 	message.buffer = NULL;
 	message.buffer_size = BUFFER_SIZE;
+	alloc_buffer();
 	ft_printf("Server PID : %d\n", getpid());
+
 	while (TRUE)
-	{
-		message.char_i = 0;
-		message.bit_i = 0;
-		message.pid_client = 0;
-		message.is_pending_pid_c = 1;
-		wait_pid_client();
-		wait_signal();
-	}
+		pause();
+
 	free(message.buffer);
 	return (0);
 }
